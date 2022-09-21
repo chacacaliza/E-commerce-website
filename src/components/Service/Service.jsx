ae75095ac5155a3d8be392724d17dd251b085e75
chacacaliza <chacacaliza99@gmail.com>
2022-09-21 10:34:22 +0700

    add service page

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import box from "../../assets/images/box.png";
+import truck from "../../assets/images/truck.png";
+import order from "../../assets/images/order.png";
+import {
+  ServicesContainer,
+  ServicesH1,
+  ServicesWrapper,
+  ServicesCard,
+  ServicesIcon,
+  ServicesH2,
+  ServicesP,
+} from "./ServiceStyles";
 
-const Service = () => {
+function service() {
   return (
-    <div>Service</div>
-  )
+    <>
+      <ServicesContainer id="services">
+        <ServicesH1>Our Services</ServicesH1>
+        <ServicesWrapper>
+          <ServicesCard>
+            <ServicesIcon src={box} />
+            <ServicesH2>Safety Package</ServicesH2>
+            <ServicesP>
+            All the safety services are available
+            </ServicesP>
+          </ServicesCard>
+          <ServicesCard>
+            <ServicesIcon src={truck} />
+            <ServicesH2>Free Delivery</ServicesH2>
+            <ServicesP>
+            100% Free Delivery 
+            </ServicesP>
+          </ServicesCard>
+          <ServicesCard>
+            <ServicesIcon src={order} />
+            <ServicesH2>Made By Order</ServicesH2>
+            <ServicesP>
+            All the products are fresh due to made by order
+            </ServicesP>
+          </ServicesCard>
+        </ServicesWrapper>
+      </ServicesContainer>
+    </>
+  );
 }
 
-export default Service
+export default service;
